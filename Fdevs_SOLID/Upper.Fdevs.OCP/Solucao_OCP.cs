b0009e653ca697265c84f6bccda8e569a98147ea
Kaique <kaiqsouza2004@hotmail.com>
2025-07-16 17:07:16 -0300

    Princípio OCP - Violação e Solução

---
+++ Fdevs_SOLID/Upper.Fdevs.OCP/Solucao_OCP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Fdevs.OCP
+{
+    // Príncipio OCP: Open-closed principle (princípio aberto-fechado)
+    // Esse princípio diz que as classes devem estar abertas para extensão, mas fechadas para modificação.
+    internal class Solucao_OCP
+    {
+            // Interface para Herança
+            public interface IClassificador
+            {
+                string ClassificarNumero(double numero);
+            }
+
+            // Abaixo estão as classes de classificações, que herdam da Interface.
+            // Nessa solução, se for necessário adicionar novas classificações posteriormente, não será necessário modificar diretamente a classe principal.
+
+            public class ParImpar : IClassificador
+            {
+                // Classifica como "Número Par" ou "Número Ímpar"
+                public string ClassificarNumero(double numero)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        return "Número Par";
+                    }
+                    else
+                    {
+                        return "Número Ímpar";
+                    }
+                }
+            }
+
+            public class PositivoNegativo : IClassificador
+            {
+                 // Classifica como "Número Positivo" ou "Número Negativo"
+                public string ClassificarNumero(double numero)
+                {
+                    if (numero >= 0)
+                    {
+                        return "Número Positivo";
+                    }
+                    else
+                    {
+                        return "Número Negativo";
+                    }
+                }
+            }
+    }
+}
---
+++ Fdevs_SOLID/Upper.Fdevs.OCP/Violacao_OCP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Fdevs.OCP
+{
+    // Príncipio OCP: Open-closed principle (princípio aberto-fechado)
+    // Esse princípio diz que as classes devem estar abertas para extensão, mas fechadas para modificação.
+    internal class Violacao_OCP
+    {
+        public class Classificador
+        {
+            // Nesse caso, o método apenas classifica números pares ou ímpares. Caso seja desejado posteriormente adicionar uma nova classificação, teria que mexer nessa classe principal.
+            public string ClassificarNumero(double numero)
+            {
+                if (numero % 2 == 0)
+                {
+                    return "Número par";
+                }
+                else
+                {
+                    return "Número ímpar";
+                }
+            }
+        }
+    }
+}
