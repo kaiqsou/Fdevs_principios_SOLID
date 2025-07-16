d5f081b103144196673cb094d56d501bf3f8ba75
Kaique <kaiqsouza2004@hotmail.com>
2025-07-16 17:55:49 -0300

    Princípio ISP - Violação e Solução

---
+++ Upper.Fdevs.ISP/Solucao_ISP.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Fdevs.ISP
+{
+    // Princípio ISP: Interface segregation principle (princípio da segregação de interface)
+    // Esse princípio afirma que nenhum cliente deve ser forçado a depender dos métodos que não usa, e para isso, interfaces maiores são 'quebradas' em interfaces menores.
+    internal class Solucao_ISP
+    {
+        // Interface para profissionais de educação
+        public interface IProfissionalEducacao
+        {
+            void DarAulas();
+        }
+
+        // Interface para profissionais de carros
+        public interface IProfissionalCarros
+        {
+            void ConsertarCarros();
+        }
+
+        // Interface para profissionais de cozinha
+        public interface IProfissionalCozinha
+        {
+            void Cozinhar();
+        }
+
+        // Classes - dessa forma, cada função de profissional recebe os seus devidos métodos, de forma bem organizada e sem métodos desnecessários.
+        public class Professor : IProfissionalEducacao
+        {
+            public void DarAulas() { }
+        }
+
+        public class Mecanico : IProfissionalCarros
+        {
+            public void ConsertarCarros() { }
+        }
+
+        public class Cozinheiro : IProfissionalCozinha
+        {
+            public void Cozinhar() { }
+        }
+
+
+    }
+}
---
+++ Upper.Fdevs.ISP/Violacao_ISP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Fdevs.ISP
+{
+    // Princípio ISP: Interface segregation principle (princípio da segregação de interface)
+    // Esse princípio afirma que nenhum cliente deve ser forçado a depender dos métodos que não usa, e para isso, interfaces maiores são 'quebradas' em interfaces menores.
+    internal class Violacao_ISP
+    {
+        public interface IProfissional
+        {
+            void DarAulas();
+            void ConsertarCarros();
+            void Cozinhar();
+        }
+
+        public class Professor : IProfissional
+        {
+            // a classe professor utilizaria apenas o método DarAulas, mas por herdar de Profissional, desnecessariamente terá que declarar "ConsertarCarros" e "Cozinhar", algo que viola o princípio ISP.
+            public void DarAulas() { }
+            public void ConsertarCarros() { }
+            public void Cozinhar() { }
+        }
+    }
+}
