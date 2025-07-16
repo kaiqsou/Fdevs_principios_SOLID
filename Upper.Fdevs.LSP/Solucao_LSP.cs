571931009b9dc207c6a84494fdf85025db806286
Kaique <kaiqsouza2004@hotmail.com>
2025-07-16 17:27:58 -0300

    Princípio LSP - Violação e Solução

---
+++ Upper.Fdevs.LSP/Solucao_LSP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Fdevs.LSP
+{
+    // Princípio LSP: Liskov substitution principle (princípio da substituição de Liskov)
+    // Esse princípio afirma que os objetos em um programa devem ser substituíveis por instâncias de seus subtipos sem alterar a correção desse programa.
+    internal class Solucao_LSP
+    {
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public string DataNascimento { get; set; }
+        }
+
+        // Dessa forma, as instâncias da classe Pessoa estão cumprindo deveres diferentes, sem métodos indevidos.
+        public class Crianca : Pessoa
+        {
+            public string Escola { get; set; }
+            public string Professor { get; set; }
+
+            public void Estudar() { } // Criança pode estudar
+        }
+
+        public class Adulto : Pessoa
+        {
+            public string Empresa { get; set; }
+            public string Cargo { get; set; }
+
+            public void Trabalhar() { } // Adulto pode trabalhar
+        }
+    }
+}
---
+++ Upper.Fdevs.LSP/Violacao_LSP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Fdevs.LSP
+{
+    // Princípio LSP: Liskov substitution principle (princípio da substituição de Liskov)
+    // Esse princípio afirma que os objetos em um programa devem ser substituíveis por instâncias de seus subtipos sem alterar a correção desse programa.
+    internal class Violacao_LSP
+    {
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public string DataNascimento { get; set; }
+
+            public void Trabalhar() {}
+        }
+
+        public class Crianca : Pessoa
+        {
+            public string Escola { get; set; }
+            public string Professor { get; set; }
+
+            // Inclui o método "Estudar" para a criança, mas ela também está herdando o método "Trabalhar" da classe Pessoa, que ela não pode ter. Isso viola o princípio LSP.
+            public void Estudar() { }
+        }
+    }
+}
