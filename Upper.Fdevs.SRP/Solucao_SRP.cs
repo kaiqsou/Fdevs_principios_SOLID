0b70dc6b193bf9e61a2e0256c9bd87d241868781
Kaique <kaiqsouza2004@hotmail.com>
2025-07-16 16:02:40 -0300

    Princípio SRP - Violação e Solução

---
+++ Upper.Fdevs.SRP/Solucao_SRP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Fdevs.SRP
+{
+    // Príncipio SRP: Single-responsibility principle (princípio de responsabilidade única)
+    // Cada módulo ou classe deve ser responsável por uma única parte do software
+    internal class Solucao_SRP
+    {
+        // Aqui, o médico terá funções apenas referentes ao seu ramo. É possível deixar ainda mais dividido, ao criar, por exemplo, uma classe de "anestesistas"
+        public class Medico
+        {
+            public string nome { get; set; }
+            public string cpf { set; get; }
+            public void Operar() { }
+            public void Anestesiar() { }
+        }
+
+        public class Financeiro
+        {
+            public string nome { get; set; }
+            public string cpf { set; get; }
+            public void EmitirRelatoriosFinanceiros() { }
+        }
+
+        public class Programador
+        {
+            public string nome { get; set; }
+            public string cpf { set; get; }
+            public void AtualizarSistema() { }
+        }
+    }
+}
---
+++ Upper.Fdevs.SRP/Violacao_SRP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Fdevs.SRP
+{
+    // Príncipio SRP: Single-responsibility principle (princípio de responsabilidade única)
+    // Cada módulo ou classe deve ser responsável por uma única parte do software
+    internal class Violacao_SRP
+    {
+        public class Medico
+        {
+            public string nome { get; set; }
+            public string cpf { set; get; }
+
+            // Aqui, está sendo atribuída ao médico muitas funções diferentes
+            public void Operar(){}
+            public void Anestesiar(){}
+            public void EmitirRelatoriosFinanceiros(){}
+            public void AtualizarSistema(){}
+
+        }
+    }
+}
